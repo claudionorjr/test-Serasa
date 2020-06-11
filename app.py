399584ad42242fe7cf9b68189ac18bc4c31ff041
claudionorjr <claudionor.junior1994@gmail.com>
2020-06-11 16:34:57 -0300

    Erros tratados com try and except

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
-import os
-import json
-from flask import Flask, render_template, redirect, request, url_for, flash
-from flask_login import LoginManager, login_required, login_user, logout_user
+from flask import Flask
+from flask_login import LoginManager
 from src.models.user import UserModel
-from src.models.company import CompanyModel
-from werkzeug.security import generate_password_hash, check_password_hash
-from werkzeug.utils import secure_filename
+from src.routes import init_routes
 
 app = Flask(__name__)
 app.config.from_pyfile('.env')
 login_manager = LoginManager(app)
+init_routes(app)
 
 @login_manager.user_loader
 def current_user(user_id):
@@ -18,125 +15,6 @@ def current_user(user_id):
 @app.before_first_request
 def create_db():
    db.create_all()
-
-@app.route("/", methods=["GET","POST"])
-def login():
-    if request.method == "POST":
-        email = request.form["email"]
-        password = request.form["password"]
-        user = UserModel.query.filter_by(email=email).first()
-
-        if not user:
-            flash("Credênciais incorretas!")
-            return redirect(url_for("login"))
-
-        if not check_password_hash(user.password, password):
-            flash("Credênciais incorretas!")
-            return redirect(url_for("login"))
-
-        login_user(user)
-        return redirect(url_for("home"))
-
-    return render_template("login.html")
-
-@app.route("/register", methods=["GET","POST"])
-def register():
-    if request.method == "POST":
-        user = UserModel()
-        user.name = request.form["name"]
-        user.email = request.form["email"]
-        user.password = generate_password_hash(request.form["password"])
-        db.session.add(user)
-        db.session.commit()
-
-        flash("Usuário criado com sucesso!")
-        return redirect(url_for("login"))
-
-    return render_template("register.html")
-
-@app.route("/current_user")
-@login_required
-def home():
-    companies = CompanyModel.query.all()
-    return render_template("home.html", companies=companies)
-
-@app.route('/current_user/uploader/<int:id>', methods = ['GET', 'POST'])
-@login_required
-def send_file(id):
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            flash('Parece que não tem arquivo!')
-            return redirect(url_for("home"))
-
-        f = request.files['file']
-        if f.filename == '':
-            flash('Não foi selecionado um arquivo!')
-            return redirect(url_for("home"))
-
-        filename = secure_filename(f.filename)
-        f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        with open(f"./data/uploads/{f.filename}", "r") as json_file:
-            daties = json.load(json_file)
-
-        company = CompanyModel.query.filter_by(id=id).first()
-        result = CompanyModel.calculator_score(daties['invoices'], daties['debits'], company)
-
-        db.session.add(result)
-        db.session.commit()
-
-        flash('Arquivo enviado com sucesso!')
-        return redirect(url_for("home"))
-
-    return render_template("home.html")
-
-@app.route("/current_user/account")
-@login_required
-def account():
-    return render_template("account.html")
-
-@app.route("/current_user/delete")
-@login_required
-def delete_confimation():
-    return render_template("delete_confimation.html")
-
-@app.route("/current_user/delete/<int:id>")
-@login_required
-def delete_user(id):
-    user = UserModel.query.filter_by(id=id).first()
-    db.session.delete(user)
-    db.session.commit()
-    logout_user()
-
-    return redirect(url_for("login"))
-
-@app.route("/logout")
-@login_required
-def logout():
-    logout_user()
-
-    flash("Logout realizado!")
-    return redirect(url_for("login"))
-
-@app.route("/current_user/create_company")
-@login_required
-def form_add_company():
-    return render_template("new_company.html")
-
-@app.route("/current_user/create_company/new", methods=["GET","POST"])
-@login_required
-def new_company():
-    if request.method == "POST":
-        company = CompanyModel()
-        company.company_name = request.form["company_name"]
-        company.invoices = 0
-        company.debits = 0
-        db.session.add(company)
-        db.session.commit()
-
-        flash("Empresa criada com sucesso!")
-        return redirect(url_for("form_add_company"))
-
-    return render_template("new_company.html")
 
 if __name__ == '__main__':
     from data.sql_alchemy import database as db
---
+++ src/routes.py
@@ -0,0 +1,140 @@
+import json
+import os
+from flask import flash, redirect, render_template, request, url_for
+from flask_login import login_required, login_user, logout_user
+from werkzeug.security import check_password_hash, generate_password_hash
+from werkzeug.utils import secure_filename
+from data.sql_alchemy import database as db
+from src.models.company import CompanyModel
+from src.models.user import UserModel
+
+
+def init_routes(app):
+    @app.route("/", methods=["GET","POST"])
+    def login():
+        if request.method == "POST":
+            email = request.form["email"]
+            password = request.form["password"]
+            user = UserModel.query.filter_by(email=email).first()
+
+            if not user:
+                flash("Credênciais incorretas!")
+                return redirect(url_for("login"))
+
+            if not check_password_hash(user.password, password):
+                flash("Credênciais incorretas!")
+                return redirect(url_for("login"))
+
+            login_user(user)
+            return redirect(url_for("home"))
+
+        return render_template("login.html")
+
+    @app.route("/register", methods=["GET","POST"])
+    def register():
+        if request.method == "POST":
+            user = UserModel()
+            user.name = request.form["name"]
+            user.email = request.form["email"]
+            user.password = generate_password_hash(request.form["password"])
+            try:
+                db.session.add(user)
+                db.session.commit()
+                flash("Usuário criado com sucesso!")
+            except:
+                flash("Erro na criação, email pode já existir!")
+
+            return redirect(url_for("login"))
+
+        return render_template("register.html")
+
+    @app.route("/current_user")
+    @login_required
+    def home():
+        companies = CompanyModel.query.all()
+        return render_template("home.html", companies=companies)
+
+    @app.route('/current_user/uploader/<int:id>', methods = ['GET', 'POST'])
+    @login_required
+    def send_file(id):
+        if request.method == 'POST':
+            if 'file' not in request.files:
+                flash('Parece que não tem arquivo!')
+                return redirect(url_for("home"))
+
+            f = request.files['file']
+            if f.filename == '':
+                flash('Não foi selecionado um arquivo!')
+                return redirect(url_for("home"))
+
+            filename = secure_filename(f.filename)
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            with open(f"./data/uploads/{f.filename}", "r") as json_file:
+                daties = json.load(json_file)
+
+            company = CompanyModel.query.filter_by(id=id).first()
+            result = CompanyModel.calculator_score(daties['invoices'], daties['debits'], company)
+            try:
+                db.session.add(result)
+                db.session.commit()
+                flash('Arquivo enviado com sucesso!')
+            except:
+                flash("Ocorreu um erro para finalizar o envio ou formato incorreto!")
+            return redirect(url_for("home"))
+
+        return render_template("home.html")
+
+    @app.route("/current_user/account")
+    @login_required
+    def account():
+        return render_template("account.html")
+
+    @app.route("/current_user/delete")
+    @login_required
+    def delete_confimation():
+        return render_template("delete_confimation.html")
+
+    @app.route("/current_user/delete/<int:id>")
+    @login_required
+    def delete_user(id):
+        user = UserModel.query.filter_by(id=id).first()
+        try:
+            db.session.delete(user)
+            db.session.commit()
+            logout_user()
+        except:
+            flash("Erro para deletar!")
+
+        return redirect(url_for("login"))
+
+    @app.route("/logout")
+    @login_required
+    def logout():
+        logout_user()
+
+        flash("Logout realizado!")
+        return redirect(url_for("login"))
+
+    @app.route("/current_user/create_company")
+    @login_required
+    def form_add_company():
+        return render_template("new_company.html")
+
+    @app.route("/current_user/create_company/new", methods=["GET","POST"])
+    @login_required
+    def new_company():
+        if request.method == "POST":
+            company = CompanyModel()
+            company.company_name = request.form["company_name"]
+            company.invoices = 0
+            company.debits = 0
+            try:
+                db.session.add(company)
+                db.session.commit()
+                flash("Empresa criada com sucesso!")
+            except:
+                flash("Erro na criação, nome pode já existir!")
+
+            return redirect(url_for("form_add_company"))
+
+        return render_template("new_company.html")
